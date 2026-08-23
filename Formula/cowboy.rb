1388dbb029120faa83b65a4ea07b3c201c6e7f1c
John Wayne <john.wayne@cowboy.inc>
2026-08-22 23:28:40 -0400

    cowboy v0.0.36 (#30)
    Co-authored-by: cowboy-cli-release <noreply@cowboy.inc>

--- Formula/cowboy.rb
+++ Formula/cowboy.rb
@@ -1,28 +1,28 @@
 class Cowboy < Formula
   desc "Cowboy blockchain command-line tool"
   homepage "https://github.com/cowboyinc/cowboy-cli"
-  version "0.0.35"
+  version "0.0.36"
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
     on_arm do
-      url "https://github.com/cowboyinc/cowboy-cli/releases/download/v0.0.35/cowboy-darwin-arm64"
-      sha256 "274a1710d17c1d7be9f4d80d421dafb44ec7f667b1d5929a5f681a34aab2d2b8"
+      url "https://github.com/cowboyinc/cowboy-cli/releases/download/v0.0.36/cowboy-darwin-arm64"
+      sha256 "5b2ca43bdcbc81b2520b8480900a0ebf043058ac570ecf2785bc9b6155e71a71"
     end
     on_intel do
-      url "https://github.com/cowboyinc/cowboy-cli/releases/download/v0.0.35/cowboy-darwin-x64"
-      sha256 "81381f6c850c08fe817e6fecb84bab1bab2d84ec57d1833aac8aed358f300487"
+      url "https://github.com/cowboyinc/cowboy-cli/releases/download/v0.0.36/cowboy-darwin-x64"
+      sha256 "f811a04f70e0131c7314b49bfbdda25404fe2089a38f3e588c85e1255e1fb18a"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/cowboyinc/cowboy-cli/releases/download/v0.0.35/cowboy-linux-arm64"
-      sha256 "12318a266c706154166076880709e49b79a509780473b02adc2ea9ee5a889c53"
+      url "https://github.com/cowboyinc/cowboy-cli/releases/download/v0.0.36/cowboy-linux-arm64"
+      sha256 "2b8a75003253434d25aa75ba00a8fd0f5c17c56f7f09ccf4cb1988b9ca32e27f"
     end
     on_intel do
-      url "https://github.com/cowboyinc/cowboy-cli/releases/download/v0.0.35/cowboy-linux-x64"
-      sha256 "ba1d79123ef48f6ef825af9a05282377ba5680242d41db0a2af6ebeb0b1a0cc7"
+      url "https://github.com/cowboyinc/cowboy-cli/releases/download/v0.0.36/cowboy-linux-x64"
+      sha256 "28027c5605301ee8fe1c8192a4bfbb0048e4b6fe5fd2bc6903d3d2557dcd98aa"
     end
   end
 
